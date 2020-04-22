f69011aa83eb3e60376c0b8187ae97ae57bc1b99
Damian <damian.cruz@rai.usc.es>
2020-04-22 16:56:02 +0200

    pruebas json damian

--- GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/IGestionDatos.java
+++ GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/IGestionDatos.java
@@ -9,5 +9,5 @@ public interface IGestionDatos {
 	public void almacenarMenu(Menu menu);
 	public void almacenarValoracion(Bandeja bandeja);
 	public void prueba(JSONObject obj);
-
+	public void prueba3(JSONObject obj);
 }
