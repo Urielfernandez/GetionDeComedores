6e86933ab02b31b211236489e3d65b8e984c9562
EnriqueCampos <enrique.campos.duran@rai.usc.es>
2020-04-22 16:38:50 +0200

    Meter JSON

--- GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/IGestionDatos.java
+++ GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/IGestionDatos.java
@@ -7,8 +7,6 @@ public interface IGestionDatos {
 	public void almacenarCompra(Bandeja bandeja);
 	public void almacenarMenu(Menu menu);
 	public void almacenarValoracion(Bandeja bandeja);
-<<<<<<< HEAD
 	//pito
-=======
->>>>>>> branch 'master' of https://github.com/Urielfernandez/GetionDeComedores.git
+
 }
--- GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/Main.java
+++ GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/Main.java
@@ -2,8 +2,7 @@ package codigoPrincipal;
 import java.util.*;
 public class Main {
 	public Main() {
-	System.out.println("S");
-	System.out.println("Prueba de funcionamiento");
-	System.out.println("Prueba de Andre");
+		System.out.println("------------ Bienvenido a KomeComSA --------------");
+		//Prueba de funciones 
 	}
 }
