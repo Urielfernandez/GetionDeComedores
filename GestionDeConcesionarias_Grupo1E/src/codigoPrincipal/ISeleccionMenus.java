c3a7bf0c8127553a7a061b2b4b365a052086dca4
AndreCampos5 <andre.campos@rai.usc.es>
2020-04-22 16:49:28 +0200

    JSON probandito voy

--- GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/ISeleccionMenus.java
+++ GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/ISeleccionMenus.java
@@ -2,6 +2,6 @@ package codigoPrincipal;
 import datos.*;
 import java.util.*;
 public interface ISeleccionMenus {
-	//aqui tendria que generar una id pa una bandeja y darsela
+ //aqui tendria que generar una id pa una bandeja y darsela
 public Bandeja seleccionarMenu(Plato primero, Plato Segundo, Plato postre, Bebida bebida);
 }
