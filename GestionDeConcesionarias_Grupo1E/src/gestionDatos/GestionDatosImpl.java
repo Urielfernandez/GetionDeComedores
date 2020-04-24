072586b6db1e77fd3ea89ea47f844832aa433976
EnriqueCampos <enrique.campos.duran@rai.usc.es>
2020-04-24 09:55:32 +0200

    Quitado un import que sobraba en gestion de datos

--- GestionDeConcesionarias_Grupo1E/src/gestionDatos/GestionDatosImpl.java
+++ GestionDeConcesionarias_Grupo1E/src/gestionDatos/GestionDatosImpl.java
@@ -2,7 +2,6 @@ package gestionDatos;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Map;
