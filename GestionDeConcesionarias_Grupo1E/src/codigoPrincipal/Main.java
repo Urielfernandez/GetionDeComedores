0de302b13fc15d7ab08fd4b461cc2282687590e3
Urielfernandez <urielfernandezgranha@gmail.com>
2020-04-24 10:07:50 +0200

    Añadida la función de de nº de bandejas no devueltas

--- GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/Main.java
+++ GestionDeConcesionarias_Grupo1E/src/codigoPrincipal/Main.java
@@ -7,6 +7,11 @@ public class Main {
 	  public static void main(String args[]) {
 			System.out.println("------------ Bienvenido a KomeComSA --------------");
 			
+			//Prueba código de PagosCualificaciones
+			
+			
+			//Prueba código de Sensores
+			ISensores sensor = new SensoresImpl();
 			
 	  }
 }
